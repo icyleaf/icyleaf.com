b0d0113527fdf01966f95fda473c42c10bd1afaa
icyleaf <icyleaf.cn@gmail.com>
2022-08-12 13:39:43 +0800

    fix: dark mode switcher invalid

--- themes/nicesima/assets/js/main.js
+++ themes/nicesima/assets/js/main.js
@@ -5,19 +5,29 @@ var _Blog = _Blog || {}
 function zip() {
   var args = [].slice.call(arguments);
   var longest = args.reduce(function(a,b){
-      return a.length>b.length ? a : b
+    return a.length>b.length ? a : b
   }, []);
 
   return longest.map(function(_,i){
-      return args.map(function(array){return array[i]})
+    return args.map(function(array){return array[i]})
   });
+}
+
+function cookieValue(key) {
+  return document.cookie
+    .split("; ")
+    .find(row => row.startsWith(`${key}=`))
+    ?.split("=")[1];
 }
 
 // Dark Mode
 _Blog.switchDarkMode = function () {
-  const currentTheme = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*=\s*([^;]*).*$)|^.*$/, '$1') || '0'
-  const isDark = currentTheme === '1' || window.matchMedia("(prefers-color-scheme: dark)").matches
-  document.cookie = 'dark=' + (isDark ? '1' : '0') + ';path=/'
+  const darkModeKey = 'blog-dark-mode'
+  const currentTheme = cookieValue(darkModeKey)
+  const isDark = (currentTheme === undefined) ? window.matchMedia("(prefers-color-scheme: dark)").matches : currentTheme === '1'
+  if (currentTheme !== undefined) {
+    document.cookie = darkModeKey + '=' + (isDark ? '1' : '0') + ';path=/'
+  }
   document.body.classList.toggle('dark-theme', isDark)
   console.log('Default dark mode is ' + isDark)
 
@@ -25,15 +35,15 @@ _Blog.switchDarkMode = function () {
   const themeSwitcher = document.querySelectorAll('.theme-switch')
   themeSwitcher.forEach(function (themeSwitcherItem) {
     themeSwitcherItem.addEventListener('click', () => {
-      const currentTheme = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*=\s*([^;]*).*$)|^.*$/, '$1') || '0'
-      const isDark = currentTheme === '1'
+      const currentTheme = cookieValue(darkModeKey)
+      const isDark = (currentTheme === undefined) ? window.matchMedia("(prefers-color-scheme: dark)").matches : currentTheme === '1'
       if (isDark) {
         document.body.classList.remove('dark-theme')
-        document.cookie = 'dark=0;path=/'
+        document.cookie = darkModeKey + '=0;path=/'
         console.log('Dark mode off')
       } else {
         document.body.classList.add('dark-theme')
-        document.cookie = 'dark=1;path=/'
+        document.cookie = darkModeKey + '=1;path=/'
         console.log('Dark mode on')
       }
     })
